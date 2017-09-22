25faf689eea2c056fbc32b113a3f6d0f36b7d747
Manuel Roman <MRoman@tradestation.com>
2017-09-21 18:39:51 -0600

    added new models

---
+++ Otis.Data/Models/Career.vb
@@ -0,0 +1,7 @@
+﻿Public Class Career
+    Property CareerId As Int32
+    Property CareerName As String
+
+    'Navigation Property
+    Property Students As ICollection(Of Student)
+End Class
---
+++ Otis.Data/Models/Student.vb
@@ -0,0 +1,16 @@
+﻿Imports System.ComponentModel.DataAnnotations
+
+Public Class Student
+    <Key>
+    Property StudentId As String
+    Property Name As String
+    Property LastName As String
+    Property SecondLastName As String
+    Property PhoneNumber As String
+    Property Address As String
+    Property EmailAddress As String
+    Property CareerId As Int32
+
+    'Navigation Property
+    Property Career As Career
+End Class
