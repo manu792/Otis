70eb01749676a6d5922bcfd1558861ad5f947929
Manuel Roman <MRoman@tradestation.com>
2017-11-21 13:33:54 -0600

    Fixed bug with logs

--- Otis.Data/Repositories/ActivityLogRepository.vb
+++ Otis.Data/Repositories/ActivityLogRepository.vb
@@ -18,15 +18,19 @@ Public Class ActivityLogRepository
     End Function
 
     Public Function GetLogsByUserAndDateRange(userId As String, FromDate As DateTime, ToDate As DateTime) As IEnumerable(Of ActivityLog)
-        Return otisContext.ActivityLogs.Include(Function(x) x.User).
-        Where(Function(x) x.UserId = userId And x.ActivityDate >= FromDate And x.ActivityDate <= ToDate).
-        ToList()
+        Return otisContext.ActivityLogs.
+            Include(Function(x) x.User.Career).
+            Include(Function(x) x.User.Profile).
+            Where(Function(x) x.UserId = userId And x.ActivityDate >= FromDate And x.ActivityDate <= ToDate).
+            ToList()
     End Function
 
     Public Function GetLogsByDateRange(FromDate As DateTime, ToDate As DateTime) As IEnumerable(Of ActivityLog)
-        Return otisContext.ActivityLogs.Include(Function(x) x.User).
-        Where(Function(x) x.ActivityDate >= FromDate And x.ActivityDate <= ToDate).
-        ToList()
+        Return otisContext.ActivityLogs.
+            Include(Function(x) x.User.Career).
+            Include(Function(x) x.User.Profile).
+            Where(Function(x) x.ActivityDate >= FromDate And x.ActivityDate <= ToDate).
+            ToList()
     End Function
 
     Public Async Function AddLog(log As ActivityLog) As Task(Of ActivityLog)
